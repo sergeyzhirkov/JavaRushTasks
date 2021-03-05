c61a334780ace6b7ec5cc7ad9db7d6612a8ac2d1
Sergey Zhirkov <zhirkov_55250@mail.ru>
2021-03-06 01:15:01 +0500

    complete tasks 2410-2412 (Anonimous Inner classes)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2411/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task24.task2411;
+
+/* 
+Вспомним наследование
+*/
+
+public class Solution extends C{
+    private class A {
+        {
+            System.out.println(getClass().getName());
+        }
+        protected String value = "A";
+
+        public A() {
+            System.out.print(value);
+        }
+    }
+
+    private A a = new A() {
+        { //у анонимных классов нет своих конструкторов, но что-то можно сделать в блоке инициализации класса
+            value = "Y";
+            if (super.getClass().getName().contains(".Solution$")) {
+                System.out.print(value);
+            }
+            {
+                System.out.println(getClass().getName());
+            }
+
+        }
+    };
+
+    public Solution() {
+        System.out.print("S");
+    }
+
+    public static void main(String[] args) {
+        new Solution();
+    }
+}
+
+
+
